1828f979e8dccc57da283f2b592b395680e5591e
Jayes <jayesbe@users.noreply.github.com>
2015-02-26 06:45:49 -0500

    Update Executive.php

--- ProcessExecutive/Executive.php
+++ ProcessExecutive/Executive.php
@@ -1,1 +1,35 @@
 <?php
+
+/*
+ * This file is part of the php-process-executive package.
+ *
+ * (c) Jayes <jayesbe@users.noreply.github.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ProcessExecutive;
+
+/**
+ * The Executive will use the provided control interface to launch child processes.
+ *
+ * @author Jayes <jayesbe@users.noreply.github.com>
+ */
+class Executive
+{
+  private 
+  
+  $control,
+  
+  $procs,
+  
+  $maxProcs;
+  
+  public function __construct(ExecutiveControl $control)
+  {
+    $this->control = $control;
+    $this->procs = array();
+    $this->maxProcs = $control->getMaxProcesses();
+  }
+}
