9b86d9ff2ac2bb617fe0c7d543c954b7f1db8411
Jayes <jayesbe@users.noreply.github.com>
2015-02-26 06:51:52 -0500

    Update ExecutiveControl.php

--- ProcessExecutive/ExecutiveControl.php
+++ ProcessExecutive/ExecutiveControl.php
@@ -1,1 +1,43 @@
 <?php
+/*
+ * This file is part of the php-process-executive package.
+ *
+ * (c) Jayes <jayesbe@users.noreply.github.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ProcessExecutive;
+
+/**
+ * Interface for the process executive to control its operation
+ *
+ * @author Jayes <jayesbe@users.noreply.github.com>
+ */
+interface ExecutiveControl
+{
+    /**
+     * Close parent resources
+     */
+    public function closeResources();
+    
+    /**
+     * Reload parent resources
+     */
+    public function reloadResources();
+    
+    /**
+     * Returns the max number of processes the executive will allow to be executed at once
+     */
+    public function getMaxProcesses();
+    
+    /**
+     * Return's an item or items to process from the queue
+     */
+    public function getProcessItem(&$queue);
+    
+    /**
+     * Executes in the child process
+     */
+    public function executeChildProcess($item);
+}
